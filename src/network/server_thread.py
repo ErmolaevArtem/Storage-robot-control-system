cf7ef1aa0529840b2b3bc6d40c803cebb14ab94a
Artem <docum.pr@gmail.com>
2025-05-29 22:09:20 +0300

    feat: create server_thread.py

--- src/network/server_thread.py
+++ src/network/server_thread.py
@@ -4,7 +4,7 @@ import socket
 
 
 class WiFiServerThread(threading.Thread):
-    def __init__(self, queue_data: queue.Queue, ip: str = '0.0.0.0', port: int = 12345):
+    def __init__(self, queue_data: queue.Queue, ip: str = '192.168.4.1', port: int = 12345):
         super().__init__()
         self.sock = None
         self.is_work = True
@@ -12,36 +12,34 @@ class WiFiServerThread(threading.Thread):
 
         self.ip = ip
         self.port = port
-        self.buffer_size: int = 16*1024
+        self.buffer_size: int = 2*1024
 
         self.queue = queue_data
 
     def run(self):
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.bind((self.ip, self.port))
+            self.sock.connect((self.ip, self.port))
             self.sock.listen(1)
-            print(f"TCP сервер запущен на {self.ip}:{self.port}")
+            print("Подключено к ESP32")
+            self.is_connection = True
         except Exception as e:
-            print(f"Ошибка при создании TCP сервера: {e}")
+            print(f"Ошибка при подключении к ESP32: {e}")
             return
 
         while self.is_work:
-            print(f"[*] Ожидание подключения...")
-            conn, addr = self.sock.accept()
-            print(f"[+] Подключено: {addr}")
-            self.is_connection = True
-
-            with conn:
-                while self.is_connection:
-                    try:
-                        data = self.sock.recv(self.buffer_size)
-                        if data:
-                            self.queue.put(data)
-                    except Exception as e:
-                        print(f"Ошибка при получении данных: {e}")
-                        self.is_connection = False
-                        continue
+            while self.is_connection:
+                try:
+                    data = self.sock.recv(self.buffer_size)
+                    if data:
+                        self.queue.put(data)
+                except Exception as e:
+                    print(f"Ошибка при получении данных: {e}")
+                    self.is_connection = False
+                    continue
+                print(len(data))
+            # здесь должна быть проверка на наличие соединения с выставлением self.is_work = False
+            # чтобы заново перезайти в run и переподключиться
 
     def stop(self):
         self.is_work = False
@@ -51,4 +49,4 @@ class WiFiServerThread(threading.Thread):
                 self.sock.close()
             except Exception as e:
                 print(f"Ошибка при закрытии сокета: {e}")
-        print("TCP сервер остановлен")
+        print("Остановлено")
